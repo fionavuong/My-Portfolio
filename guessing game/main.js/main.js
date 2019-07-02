2c92cbd1ce51f3394eb34ed869b9df33fd392735
Mission Bit <missionbit@31a-MissionBit.local>
2019-07-02 09:53:35 -0700

    bklb

---
+++ guessing game/main.js/main.js
@@ -0,0 +1,13 @@
+var guess = prompt('Enter a number')
+
+guess = Number(guess)
+
+for(i = 0; <= guess; i++) {
+  console.log(i)
+}
+
+var myList = ['hi', 'cool', 'die']
+
+for (p in myList) {
+    console.log(myList[p])
+}
